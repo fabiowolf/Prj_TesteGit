4ac90123da68cc13db412474c09cb4ce1903c973
DEV_MACHINE <DEV_MACHINE@10.0.2.15>
2013-06-23 18:15:21 -0300

    Teste de Extração...

--- src/prj_testegit/Prj_TesteGit.java
+++ src/prj_testegit/Prj_TesteGit.java
@@ -16,5 +16,6 @@ public class Prj_TesteGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Teste...");
+        System.out.println("Teste3");
     }
 }
